30e6cee471dac941a68227026fc82b18db22e9e7
Ilya Prokshin <prokshin99@yandex.ru>
2019-12-02 09:03:30 +0500

    Add simple route logic in TasksPage

--- src/component/content/content.js
+++ src/component/content/content.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./content.css";
+import TasksPage from "../../pages/tasks";
 
 const Content = props => {
-  return <div className="content">{props.type}</div>;
+  const id = 1;
+  let element = "";
+  switch (props.type) {
+    case "my task page":
+      element = <TasksPage types="Страница с тестами и заданиями" />;
+      break;
+    default:
+      element = <div>{props.type}</div>;
+      break;
+  }
+  return <div className="content">{element}</div>;
 };
 
 export default Content;
---
+++ src/pages/tasks.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+
+export default class TasksPage extends Component {
+  back() {
+    window.history.back();
+  }
+  render() {
+    let id = 0;
+    return (
+      <Router>
+        <Switch>
+          <Route path={`/tasks/test_${id}`}>
+            <button onClick={this.back}>Закрыть тест</button>
+            <div>
+              тест, расположенный по адресу /tasks/test_id , где id - уникальный
+              идентификатор теста
+            </div>
+          </Route>
+          <Route path={`/tasks`}>
+            <div>{this.props.types}</div>
+            <Link to={`/tasks/test_${id}`}>Ссылка на тест</Link>
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
+}
